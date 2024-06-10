fc6925d1db35f7cb30fdf790390ced5ec141cb98
Fredrik Granvik <fredrik.granvik@fortnox.se>
2024-06-10 22:54:36 +0200

    fix

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -2,7 +2,7 @@
 import { HeroPlaceholder } from "../HeroPlaceholder/heroPlaceholder";
 import Margin from "../Margin/margin";
 // import { MediaList } from "../MediaList/mediaList";
-import { Section } from "../Section/section";
+// import { Section } from "../Section/section";
 import styles from "./main.module.css";
 
 export const Main: React.FC = () => {

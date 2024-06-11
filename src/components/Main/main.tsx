34935078da41364f76d17fcdf810104fd0119269
Fredrik Granvik <fredrik.granvik@fortnox.se>
2024-06-11 16:58:02 +0200

    removed unused code

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,10 +1,4 @@
-// import { Hero } from "../Hero/hero";
 import { PropsWithChildren } from "react";
-import { HeroPlaceholder } from "../HeroPlaceholder/heroPlaceholder";
-import Margin from "../Margin/margin";
-// import { MediaList } from "../MediaList/mediaList";
-// import { Section } from "../Section/section";
-import styles from "./main.module.css";
 
 export const Main: React.FC<PropsWithChildren> = ({ children }) => {
   return <>{children}</>;

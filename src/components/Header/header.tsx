e36daae70ab09cd3950fe9861de5a0f2240d7be0
Fredrik Granvik <fredrik.granvik@fortnox.se>
2024-06-12 08:24:23 +0200

    added missing aria label to header logo

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <Link to="/">
+      <Link to="/" aria-label="Go to the startpage of squeakyadventures.com">
         <Logo className={styles.logo} />
       </Link>
       <HeaderMenu />

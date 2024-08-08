a67810c1644763e1e1fd93465813b720f5a61e7d
Fredrik Granvik <fredrik.granvik@fortnox.se>
2024-08-08 10:20:25 +0200

    Fix case sensitivity issues

--- src/components/Section/section.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import styles from "./Section.module.css";
-
-interface Prop {}
-
-export const Section: React.FC<PropsWithChildren<Prop>> = ({ children }) => {
-  return <section className={styles.section}>{children}</section>;
-};
